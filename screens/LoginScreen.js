296912ce2b0a6fe6e39c9359f59382c7b17a0f16
= <=>
2021-02-01 12:14:03 +0100

    010221:1213

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -11,7 +11,7 @@ const LoginScreen = ({navigation}) => {
 
   const  signin = () =>{
 
-    console.log("login");
+   // console.log("login");
     console.log(email);
     console.log(password);
     }
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,14 +1,104 @@
-import React from 'react'
-import { View, Text,StyleSheet } from 'react-native'
+import React,{useState,useLayoutEffect} from 'react';
+import { KeyboardAvoidingView,StyleSheet,View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import {Button,Input,Text} from 'react-native-elements';
+
 
 const RegisterScreen = ({navigation}) => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+
+    useLayoutEffect(() => {
+       
+        navigation.setOptions({
+
+            headerTitle:"Login",
+           
+             
+        });
+    }, [navigation]);
+
+
+    const register=()=>{
+
+    
+
+    }
+
+
     return (
-        <View>
-            <Text>register</Text>
-        </View>
+        <KeyboardAvoidingView behavior='padding' style={styles.container}>
+
+        <StatusBar style="light" />
+
+            <Text h3 style={{marginBottom:50}}> Create Signal Account</Text>
+
+            <View style={styles.inputContainer}>
+
+            <Input  placeholder="Full name" 
+            autoFocus 
+            type="text"
+            value={name}
+            onChangeText={(text)=>{setName(text)}}
+             />
+
+            <Input  placeholder="Email" 
+             
+            type="email"
+            value={email}
+            onChangeText={(text)=>{setEmail(text)}}
+             />
+
+            <Input  placeholder="Password" 
+             
+            type="password"
+            value={password}
+            secureTextEntry
+            onChangeText={(text)=>{setPassword(text)}}
+             />
+
+            <Input  placeholder="Image URL" 
+             
+            type="text"
+            value={imageUrl}
+            onChangeText={(text)=>{setImageUrl(text)}}
+            onSubmitEditing={register}
+             />
+
+            </View>
+
+
+            <Button containerStyle={styles.button} raised title="Register" onPress={register} />
+
+            <View style={{height:100}} />
+
+        </KeyboardAvoidingView>
     )
 }
 
 export default RegisterScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+    container:{
+
+        flex:1,
+        alignItems:"center",
+        justifyContent: 'center',
+        padding:10,
+        backgroundColor:"white"
+    },
+
+    inputContainer:{
+        width:300,
+       
+    },
+
+    button:{
+        width:200,
+        marginTop:10
+    }
+})
