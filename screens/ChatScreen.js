937267efa08342e371dbd1d6f294863acd188bbc
= <=>
2021-02-03 11:14:27 +0100

    030221:1114

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,8 +1,10 @@
 import React,{useLayoutEffect,useState} from 'react';
-import { View, Text,StyleSheet,SafeAreaView,StatusBar,KeyboardAvoidingView, Platform,ScrollView,TextInput} from 'react-native';
+import { View, Text,StyleSheet,SafeAreaView,StatusBar,KeyboardAvoidingView, Platform,ScrollView,TextInput,Keyboard,TouchableWithoutFeedback} from 'react-native';
 import { Avatar } from 'react-native-elements';
 import { TouchableOpacity } from 'react-native';
 import {AntDesign,Ionicons,FontAwesome} from "@expo/vector-icons";
+import {auth, db} from '../firebase';
+import * as firebase from 'firebase';
 
 const ChatScreen = ({route,navigation}) => {
 
@@ -10,8 +12,8 @@ const ChatScreen = ({route,navigation}) => {
 
 
     const [input, setInput] = useState('');
-
-
+    const [messages, setMessages] = useState([]);
+ 
 
     useLayoutEffect(() => {
        
@@ -76,9 +78,35 @@ const ChatScreen = ({route,navigation}) => {
 
     const sendMessage=()=>{
 
+        Keyboard.dismiss();
+        db.collection('chats').doc(id).collection('messages').add({
+
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            message:input,
+            displayName:auth.currentUser.displayName,
+            email:auth.currentUser.email,
+            photoURL: auth.currentUser.photoURL
+        })
+
+        setInput('')
 
     }
 
+    useLayoutEffect(() => {
+       
+        const unsuscribe = db.collection('chats').doc(id).collection('messages')
+        .orderBy('timestamp','desc').onSnapshot((snapshot)=>{
+
+            setMessages(snapshot.docs.map(doc=>({
+
+                idx:doc.id,
+                data:doc.data()
+            })))
+        })
+
+        return unsuscribe
+
+    }, [route])
 
     return (
         <SafeAreaView style={{flex:1,backgroundColor:'white'}}> 
@@ -92,10 +120,43 @@ const ChatScreen = ({route,navigation}) => {
                 keyboardVerticalOffset={90}
            >
 
-                <>
+           <TouchableWithoutFeedback onPress={Keyboard.dismiss} >
+
+            <>
                     <ScrollView>
 
+                    {messages.map(({idx,data})=>(
 
+                        data.email===auth.currentUser.email ? (
+
+                              <View key={idx} style={styles.receiver}>
+
+                                <Avatar 
+                                rounded
+                                size={30}
+                                source={{
+                                    uri:data.photoURL
+                                }}
+
+                                />
+                                <Text style={styles.receiverText}>{data.message}</Text>
+
+                              </View>
+
+
+                        ):(
+
+                            <View style={styles.sender}>
+                                
+                            <Avatar />
+                            <Text style={styles.senderText}>{data.message}</Text>
+
+                            </View>
+
+                        )
+
+                    ))}
+                  
                     </ScrollView>
 
                     <View style={styles.footer}>
@@ -106,6 +167,7 @@ const ChatScreen = ({route,navigation}) => {
                         style={styles.input}
                         value={input}
                         onChangeText={(text)=>setInput(text)}
+                        onSubmitEditing={sendMessage}
                          />
 
                          <TouchableOpacity onPress={sendMessage} activeOpacity={0.5}>
@@ -117,7 +179,9 @@ const ChatScreen = ({route,navigation}) => {
 
                     </View>
 
-                </>
+                    </>
+                    </TouchableWithoutFeedback>
+
 
            </KeyboardAvoidingView>
           
@@ -130,15 +194,62 @@ export default ChatScreen
 const styles = StyleSheet.create({
 
     container:{
-
+        flex:1
     },
 
     footer:{
-
+        flexDirection:'row',
+        alignItems:'center',
+        width:'100%',
+        padding:15
 
     },
 
     input:{
+
+        bottom:0,
+       // borderColor:'transparent',
+        height:40,
+        flex:1,
+        marginRight:15,
+        backgroundColor:'#ECECEC',
+        //borderWidth:1,
+        padding:10,
+        color:'grey',
+        borderRadius:30
+    },
+
+    receiverText:{
+
+
+    },
+
+    senderText:{
+
+
+    },
+
+    receiver:{
+
+        padding:15,
+        backgroundColor:'#ECECEC',
+        alignSelf:'flex-end',
+        borderRadius:20,
+        marginRight:15,
+        marginBottom:20,
+        maxWidth:'80%',
+        position:'relative'
+    },
+
+    sender:{
+
+        padding:15,
+        backgroundColor:'#2B68E6',
+        alignSelf:'flex-start',
+        borderRadius:20,
+        margin:15,
+        maxWidth:'80%',
+        position:'relative'
 
     }
 })
